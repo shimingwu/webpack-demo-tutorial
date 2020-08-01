4b75175c17a381808999448c9eec774838d79f9f
wu_shiming <wushiming1027@gmail.com>
2020-08-01 15:16:06 +0800

    add sample code for demo purpose

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # webpack-demo-tutorial
-This is a repository for demo purpose. It is for the tutorial created to learn how to setup React, webpack and babel from scratch.
+This is a repository for the tutorial created to learn how to setup React, webpack and babel from scratch.
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path'); // node-js
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    },
+                    {   
+                        loader: "css-loader"
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: __dirname + '/src/index.html',
+            filename: 'index.html',
+            inject: 'body'
+        })
+    ]
+}
